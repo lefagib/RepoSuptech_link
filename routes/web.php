914f8e2bba67285eb7ec5565c49cf4a280bff53b
Magib <magib.fall42@gmail.com>
2024-01-26 20:06:18 +0000

    donnes_fresh2

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/user', function () {
    Roles::create([
     'name' => "admin",
     'Description' => "Mon super admin"]);
